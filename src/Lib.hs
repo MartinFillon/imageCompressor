fb08d5e3b5211c8845f8930d33870afd37107d34
MartinFillon <martin.fillon@hotmail.com>
2024-03-04 14:23:03 +0100

    feat: format code and add formatter fourmolu

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+{-
+-- EPITECH PROJECT, 2024
+-- Wolfram
+-- File description:
+-- Lib
+-}
+
+module Lib (
+    someFunc,
+) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
