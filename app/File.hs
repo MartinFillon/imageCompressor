1a2a5e4202c82bc5ff55073d241ba5317bc95707
MartinFillon <martin.fillon@hotmail.com>
2024-03-06 09:37:34 +0100

    feat: file is parsed

--- app/File.hs
+++ app/File.hs
@@ -5,12 +5,24 @@
 -- Main
 -}
 
-module File (openFile) where
+module File (openFile, parseFile) where
 
-import System.IO (readFile)
+import Data.Functor
+import Text.Read (readMaybe)
 
+import Colors (Color)
 import Lib (pError)
+import Points (Point)
 
-openFile :: Maybe String -> IO (String)
-openFile Nothing = pError "File not specified" >> return "a"
-openFile (Just p) = readFile p
+openFile :: Maybe String -> IO [String]
+openFile Nothing = pError "File not specified" >> return []
+openFile (Just p) = readFile p <&> lines
+
+splitAtFirst :: Eq a => a -> [a] -> ([a], [a])
+splitAtFirst x = fmap (drop 1) . break (x ==)
+
+parseFile :: [String] -> [(Maybe Point, Maybe Color)]
+parseFile = map (parseLine . splitAtFirst ' ')
+
+parseLine :: (String, String) -> (Maybe Point, Maybe Color)
+parseLine (a, b) = (readMaybe a, readMaybe b)
