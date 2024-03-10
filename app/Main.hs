5804a065c220617ef36b15c9363e6a3f6bbf131d
louonezime <lou.onezime@epitech.eu>
2024-03-10 18:56:29 +0100

    fix: fixed coding-style errors from martin

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,7 @@ import Points
 
 printFileContent :: Maybe Opt -> IO ()
 printFileContent Nothing = return ()
-printFileContent (Just (Opt r _ _)) = openFile r >>= print . (\x -> tryRun (parseFile x))
+printFileContent (Just (Opt r _ _)) = openFile r >>= print . run
 
 splitIt :: [(Point, Color)] -> [Color]
 splitIt [] = []
@@ -26,6 +26,9 @@ splitIt ((_, c) : xs) = c : splitIt xs
 tryRun :: Either String [(Point, Color)] -> [[Color]]
 tryRun (Left _) = []
 tryRun (Right l) = kMeans (splitIt l) 2 0.0 [Color 1 2 3, Color 3 2 1]
+
+run :: [String] -> [[Color]]
+run = tryRun . parseFile
 
 main :: IO ()
 main = getArgs >>= printFileContent . optParser defaultOpt
