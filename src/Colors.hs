b13e40be1b76309ed53037c58775ffdc20a42764
MartinFillon <martin.fillon@hotmail.com>
2024-03-22 11:54:35 +0100

    fix: using int instead of Word8
    Thanks Marc

--- src/Colors.hs
+++ src/Colors.hs
@@ -10,29 +10,36 @@ module Colors (Color (..), colorFrom, distance, printColor) where
 import Data.Word (Word8)
 
 data Color = Color
-    { r :: Word8,
-      g :: Word8,
-      b :: Word8
+    { r :: Int,
+      g :: Int,
+      b :: Int
     }
     deriving (Eq, Show)
 
-colorFrom :: (Word8, Word8, Word8) -> Color
+colorFrom :: (Int, Int, Int) -> Color
 colorFrom (red, green, blue) = Color red green blue
 
 printColor :: Color -> IO ()
 printColor (Color 255 255 255) = return ()
-printColor (Color r g b) = putStrLn $ "(" ++ show r ++ ", " ++
-  show g ++ ", " ++ show b ++ ")"
+printColor (Color r g b) =
+    putStrLn $
+        "("
+            ++ show r
+            ++ ", "
+            ++ show g
+            ++ ", "
+            ++ show b
+            ++ ")"
 
-instance Read Color where
-    readsPrec _ str =
-        (\(red, green, blue) -> [(colorFrom (red, green, blue), "")])
-            (read str :: (Word8, Word8, Word8))
+-- instance Read Color where
+--     readsPrec _ str =
+--         (\(red, green, blue) -> [(colorFrom (red, green, blue), "")])
+--             (read str :: (Word8, Word8, Word8))
 
-distance' :: Word8 -> Word8 -> Float
+distance' :: Int -> Int -> Float
 distance' x y = fromIntegral ((x - y) ^ 2) :: Float
 
-distance :: Color -> Color -> Float
-distance (Color xr xg xb) (Color yr yg yb) = sqrt dist
+distance :: (Int, Int, Int) -> (Int, Int, Int) -> Float
+distance (xr, xg, xb) (yr, yg, yb) = sqrt dist
   where
     dist = distance' yr xr + distance' yg xg + distance' yb xb
